60625722896bfa3b282074fc0de84c0e4b48b13f
bozhidar.slavov <bozhidar.slavov96@gmail.com>
2016-10-02 23:13:32 +0300

    Promises and asynchronous programming tasks

---
+++ 01.Promises-and-Asynchronous-Programming/01.GeoLocation/task-1.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    function getGeoLocation() {
+        let promise = new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(position => {
+                resolve(position);
+            });
+        });
+
+        return promise;
+    }
+
+    function showGeolocationImage(position) {
+        let latitude = position.coords.latitude,
+            longitude = position.coords.longitude,
+            image = new Image();
+
+        image.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+        document.body.appendChild(image);
+    }
+
+    getGeoLocation()
+        .then(success => {
+            showGeolocationImage(success);
+        }, error => {
+            console.error(error);
+        });
+}());
---
+++ 01.Promises-and-Asynchronous-Programming/02.PopupThenLoad/task-2.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    const milisecondsBeforeRedirect = 2000;
+
+    let box = document.getElementById('popup');
+
+    function timeout() {
+        let promise = new Promise((resolve, reject) => {
+            return setTimeout(() => {
+                resolve({});
+            }, milisecondsBeforeRedirect);
+        });
+
+        return promise;
+    }
+
+    let button = document.getElementById('btn');
+
+    button.addEventListener('click', event => {
+        timeout()
+            .then(success => {
+                window.location = "https://google.com/";
+            });
+    });
+}());
